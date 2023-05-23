9cf1b36be74052d5a7720b785917069ec2289d27
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-23 10:42:25 -0500

    feat: buttons added

--- lib/src/pages/start_page.dart
+++ lib/src/pages/start_page.dart
@@ -6,7 +6,39 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Vital'),
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+          _getButton('Iniciar Sesión', Color.fromRGBO(29, 53, 87, 1), context,
+              'login'),
+          _getButton('Registrarse', Colors.white, context, 'register',
+              borderColor: Color.fromRGBO(29, 53, 87, 1),
+              textColor: Color.fromRGBO(29, 53, 87, 1))
+        ]),
+      )),
+    );
+  }
+
+  _getButton(String label, color, BuildContext context, String route,
+      {borderColor = Colors.white, textColor = Colors.white}) {
+    return ElevatedButton(
+      onPressed: () => {Navigator.pushNamed(context, route)},
+      child: Text(label),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        foregroundColor: textColor,
+        minimumSize: Size(250, 40),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: BorderSide(color: borderColor, width: 1.0)),
+      ),
+    );
+  }
+
+  _createImage() {
+    return Expanded(
+      child: Image(fit: BoxFit.contain, image: AssetImage('logo.jpg')),
     );
   }
 }

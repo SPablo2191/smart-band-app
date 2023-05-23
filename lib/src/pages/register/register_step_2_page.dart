2ed5622fcdfd18971a289bb199e0cb5746c6c5f2
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-23 17:49:01 -0500

    feat: register step 2 scaffold

--- lib/src/pages/register/register_step_2_page.dart
+++ lib/src/pages/register/register_step_2_page.dart
@@ -10,8 +10,88 @@ class RegisterStep2Page extends StatefulWidget {
 class _RegisterStep2PageState extends State<RegisterStep2Page> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hola'),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Center(
+            child: Text(
+          'Registrarse',
+          style: TextStyle(color: Color.fromRGBO(29, 53, 87, 1), fontSize: 30),
+        )),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+          children: [
+            Container(height: 100),
+            Container(
+              height: 15,
+            ),
+            Container(
+              height: 15,
+            ),
+            Container(
+              height: 40,
+            ),
+            _getButton('Siguiente', Color.fromRGBO(29, 53, 87, 1), context,
+                'register/step2'),
+            Container(
+                margin: EdgeInsets.only(top: 46.0),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+                child: Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, 'login');
+                        },
+                        child: RichText(
+                          text: const TextSpan(
+                            text: '¿Ya tienes cuenta? ',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'Inicia Sesión',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  color: Color.fromRGBO(230, 57, 70, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )))
+          ]),
+    );
+  }
+
+  _getButton(String label, color, BuildContext context, String route,
+      {borderColor = Colors.white, textColor = Colors.white}) {
+    return ElevatedButton(
+      onPressed: () => {Navigator.pushNamed(context, route)},
+      child: Text(
+        label,
+        style: TextStyle(fontSize: 20),
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        foregroundColor: textColor,
+        minimumSize: Size(250, 50),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: BorderSide(color: borderColor, width: 1.0)),
+      ),
     );
   }
 }

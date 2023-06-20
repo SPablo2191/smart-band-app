d5bf2bff20ff852d04b7a95935a158f53e31fd02
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-20 17:59:15 -0500

    feat: home button with padding

--- lib/src/pages/student_page.dart
+++ lib/src/pages/student_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smartband/src/core/consts/colors.dart';
+import 'package:smartband/src/widgets/appbar_widget.dart';
+import 'package:smartband/src/widgets/ui/ui_button_widget.dart';
 
 class StudentPage extends StatefulWidget {
   const StudentPage({super.key});
@@ -10,6 +13,32 @@ class StudentPage extends StatefulWidget {
 class _StudentPageState extends State<StudentPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: const CustomAppBar(
+          title: 'Ver Estudiantes',
+          showBackButton: true,
+          centerTitle: false,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 20.0, right: 15, left: 15),
+          child: ListView(
+            children: [
+              UiButton(
+                label: 'Crear Estudiante',
+                color: colorSecondary,
+                textColor: colorPrimary,
+                context: context,
+                onPressedCallback: () => {},
+              ),
+              UiButton(
+                label: 'Crear Estudiante',
+                color: colorSecondary,
+                textColor: colorPrimary,
+                context: context,
+                onPressedCallback: () => {},
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/src/widgets/home_button_widget.dart
+++ lib/src/widgets/home_button_widget.dart
@@ -7,6 +7,10 @@ class HomeButton extends StatelessWidget {
   final Color buttonLabelColor;
   final String buttonLabel;
   final String path;
+  final double top;
+  final double left;
+  final double right;
+  final double bottom;
 
   const HomeButton(
       {required this.buttonColor,
@@ -15,37 +19,45 @@ class HomeButton extends StatelessWidget {
       required this.buttonLabel,
       required this.buttonLabelColor,
       required this.path,
+      this.top = 1.0,
+      this.left = 1.0,
+      this.bottom = 1.0,
+      this.right = 1.0,
       super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: buttonColor,
-          ),
-          child: IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, path);
-            },
-            iconSize: 30,
-            icon: Icon(
-              buttonIcon,
-              color: buttonIconColor,
+    return Padding(
+      padding:
+          EdgeInsets.only(top: top, bottom: bottom, left: left, right: right),
+      child: Column(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: buttonColor,
+            ),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, path);
+              },
+              iconSize: 30,
+              icon: Icon(
+                buttonIcon,
+                color: buttonIconColor,
+              ),
             ),
           ),
-        ),
-        const SizedBox(height: 8),
-        Text(
-          buttonLabel,
-          textAlign: TextAlign.center,
-          style: TextStyle(color: buttonLabelColor),
-        ),
-      ],
+          const SizedBox(height: 8),
+          Text(
+            buttonLabel,
+            textAlign: TextAlign.center,
+            style: TextStyle(color: buttonLabelColor),
+          ),
+        ],
+      ),
     );
   }
 }

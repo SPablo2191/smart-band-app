f7a8a343606075921f8591776d6376c161d42022
DIEGOPARDO22 <diego.pardo@ucsm.edu.pe>
2023-06-21 12:06:24 -0500

    add : route a create promotion

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,11 +3,12 @@ import 'dart:js';
 import 'package:flutter/material.dart';
 import 'package:smartband/src/pages/home_page.dart';
 import 'package:smartband/src/pages/profile_page.dart';
-import 'package:smartband/src/pages/promotion_page.dart';
+import 'package:smartband/src/pages/promotion/promotion_page.dart';
 import 'package:smartband/src/pages/register/register_step_2_page.dart';
 import 'package:smartband/src/pages/start_page.dart';
 
 import '../pages/login_page.dart';
+import '../pages/promotion/promotion_create_page.dart';
 import '../pages/register/register_step_1_page.dart';
 
 Map<String, WidgetBuilder> getRoutes() {
@@ -18,6 +19,7 @@ Map<String, WidgetBuilder> getRoutes() {
     'register/step2': (BuildContext context) => const RegisterStep2Page(),
     'home': (BuildContext context) => const HomePage(),
     'profile': (BuildContext context) => const ProfilePage(),
-    'promotion': (context) => const PromotionPage()
+    'promotion': (context) => const PromotionPage(),
+     'promotion/create': (BuildContext context) => const PromotionCreate_page(),
   };
 }

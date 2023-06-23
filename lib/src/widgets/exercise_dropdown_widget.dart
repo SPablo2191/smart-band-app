669d7bebe285e87c53954775d5d6e47645f02643
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-23 15:19:38 -0500

    feat: exercise dropdown

---
+++ lib/src/widgets/exercise_dropdown_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:vital/src/core/consts/colors.dart';
+import 'package:vital/src/models/exercise_model.dart';
+import 'package:vital/src/providers/exercise_provider.dart';
+
+class ExerciseDropdown extends StatefulWidget {
+  final String accessToken;
+  final Function(Exercise?) onExerciseSelected;
+
+  const ExerciseDropdown({
+    required this.accessToken,
+    required this.onExerciseSelected,
+  });
+
+  @override
+  _ExerciseDropdownState createState() => _ExerciseDropdownState();
+}
+
+class _ExerciseDropdownState extends State<ExerciseDropdown> {
+  final ExerciseProvider exerciseProvider = ExerciseProvider();
+  List<Exercise> exercises = [];
+  Exercise? selectedExercise;
+
+  @override
+  void initState() {
+    super.initState();
+    loadExercises();
+  }
+
+  void loadExercises() async {
+    List<Exercise> fetchedExercises =
+        await exerciseProvider.getExercises(widget.accessToken);
+
+    setState(() {
+      exercises = fetchedExercises;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 12.0, bottom: 8.0),
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: colorLight,
+          border: Border.all(
+            color: colorPrimary,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: DropdownButton<Exercise>(
+          value: selectedExercise,
+          onChanged: (Exercise? newValue) {
+            setState(() {
+              selectedExercise = newValue;
+            });
+
+            widget.onExerciseSelected(newValue);
+          },
+          hint: const Text(
+            'Seleccionar un ejercicio',
+            style: TextStyle(color: colorPrimary),
+          ),
+          dropdownColor: colorSecondary,
+          icon: const Icon(Icons.fitness_center_outlined),
+          items: exercises.map<DropdownMenuItem<Exercise>>((Exercise value) {
+            return DropdownMenuItem<Exercise>(
+              value: value,
+              child: Text(
+                value.description ?? '',
+                style: const TextStyle(color: colorPrimary),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}

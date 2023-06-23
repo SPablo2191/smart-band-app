c35c92364891725447e88d83e1bcf007fa4042a0
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-23 13:00:25 -0500

    feat: select school added

--- lib/src/pages/test/add_test_page.dart
+++ lib/src/pages/test/add_test_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:vital/src/widgets/appbar_widget.dart';
 import 'package:vital/src/widgets/bottom_navigation_bar_widget.dart';
+import 'package:vital/src/widgets/school_dropdown_widget.dart';
+
+import '../../models/school_model.dart';
 
 class TestAddPage extends StatefulWidget {
   const TestAddPage({super.key});
@@ -10,11 +13,21 @@ class TestAddPage extends StatefulWidget {
 }
 
 class _TestAddPageState extends State<TestAddPage> {
+  School? selectedSchool;
+  void onSchoolSelected(School? school) {
+    setState(() {
+      selectedSchool = school;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(
           title: 'Crear Evaluación', centerTitle: false, showBackButton: true),
+      body: ListView(
+        children: [SchoolDropdown(onSchoolSelected: onSchoolSelected)],
+      ),
       bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
